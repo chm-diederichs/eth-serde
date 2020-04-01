cffdb5d15977b1aff904370cfc5798de647d8560
Christophe Diederichs <chris.d.itunes@gmail.com>
2020-04-02 00:03:41 +0200

    remove methods needing packed encoding

--- abi/index.js
+++ abi/index.js
@@ -9,10 +9,7 @@ module.exports = {
   decode,
   encodingLength,
   encodeConstructor,
-  encodeMethod,
-  encodeLocal,
-  encodeMethodExternal,
-  encodeMethodType
+  encodeMethod
 }
 
 function encode (signature, args, buf, offset) {
@@ -148,44 +145,6 @@ function encodeMethod (name, signature, args) {
   buf.set(methodID(name, signature))
 
   return encode(signature, args, buf, 4)
-}
-
-function encodeLocal (contractAddress, method) {
-  assert(typeof contractAddress === 'string', 'contractAddress must be string')
-  assert(typeof method === 'string', 'method must be string')
-
-  return pack(
-    ['address', 'string'],
-    [contractAddress, method]
-  )
-}
-
-function encodeMethodExternal (contractAddress, contract, method, signature) {
-  assert(typeof contractAddress === 'string', 'contractAddress must be string')
-  assert(contractAddress.length === 42, 'contractAddress must be an ethereum address')
-  assert(typeof contract === 'string', 'contract must be string')
-  assert(contract.length === 42, 'contract must be 42 bytes (160 bit)')
-  assert(typeof method === 'string', 'method must be string')
-  assert(Array.isArray(signature), 'signature must be an array of solidity types')
-
-  return pack(
-    ['bytes', 'address', 'bytes4'],
-    [encodeLocal(contractAddress, 'execute'), contract, methodID(method, signature)]
-  )
-}
-
-function encodeMethodType (contractAddress, codehash, method, signature) {
-  assert(typeof contractAddress === 'string', 'contractAddress must be string')
-  assert(contractAddress.length === 42, 'contractAddress must be an ethereum address')
-  assert(typeof codehash === 'string', 'codehash must be string')
-  assert(codehash.length === 66, 'codehash must be 66 bytes (256 bit)')
-  assert(typeof method === 'string', 'method must be string')
-  assert(Array.isArray(signature), 'signature must be an array of solidity types')
-
-  return pack(
-    ['bytes', 'bytes32', 'bytes4'],
-    [encodeLocal(contractAddress, 'executeType'), codehash, methodID(method, signature)]
-  )
 }
 
 function decodeOutput (signature, data) {
